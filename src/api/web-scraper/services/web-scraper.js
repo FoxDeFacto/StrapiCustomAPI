7b42d0927b148161bb551097d7b13440c09366e6
FoxDeFacto <lisakzezhore@seznam.cz>
2024-05-28 16:04:04 +0200

    Add scraping for Steam

--- src/api/web-scraper/services/web-scraper.js
+++ src/api/web-scraper/services/web-scraper.js
@@ -1,24 +1,25 @@
 const axios = require('axios');
 const { JSDOM } = require('jsdom');
-const cheerio = require('cheerio');
+const validUrl = require('valid-url');
+const puppeteer = require('puppeteer');
 
-//Odebere escape sekvence a nové řádky
+// Odebere escape sekvence a nové řádky
 const cleanText = (text) => text.replace(/[\n\t]+/g, ' ').replace(/\s{2,}/g, ' ').trim();
 
 const splitTextByKeywords = (text) => {
-  //Pole klíčových slov
+  // Pole klíčových slov
   const keywords = ['Tvůrci', 'Režie', 'Scénář', 'Kamera', 'Hudba', 'Hrají'];
-  //Tvorba reger vzoru na dělení dle klíčových slov
+  // Tvorba regex vzoru na dělení dle klíčových slov
   const regexPattern = new RegExp(`\\s*(${keywords.join('|')})\\s*`, 'g');
-  //Dělení vstupu a odfiltrování prázdných výstupů
+  // Dělení vstupu a odfiltrování prázdných výstupů
   const splitText = text.split(regexPattern).filter(Boolean);
-  //Spojení výstupů do pole ve formátu [Klíč, Hodnota]
+  // Spojení výstupů do pole ve formátu [Klíč, Hodnota]
   let result = {};
   for (let i = 0; i < splitText.length; i += 2) {
     const key = splitText[i];
     const value = splitText[i + 1];
     if (key && value) {
-      result[key] = value.length > 2 ? value.substring(2).trim() : ''; //Odebere první 2 znaky na začátku výstupu
+      result[key] = value.length > 2 ? value.substring(2).trim() : ''; // Odebere první 2 znaky na začátku výstupu
     }
   }
   return result;
@@ -43,101 +44,85 @@ const handleCsfdData = (document) => {
 
 // Funkce na zpracování dat z MyAnimeListu
 const handleMalData = (document) => {
-
   const rating = cleanText(document.querySelector('.score-label').textContent);
   const animeName = cleanText(document.querySelector('.title-name.h1_bold_none').textContent);
   const storySummary = cleanText(document.querySelector('[itemprop="description"]').textContent);
 
   return { 
-    rating,
     animeName,
+    rating,
     storySummary
   };
 };
 
-// Funkce na zpracování dat z Steam
+// Funkce na zpracování dat ze Steam
 const handleSteamData = async (url) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
 
-  const  result  = await fetchAgeVerifiedPage(url);
-  //console.log(result);
-  //const  { obj }  = await fetchAgeVerifiedPage(url);
-  //const  document = new JSDOM(obj).window;
-  //const out = document.querySelector('html');
+  //Počká na vykreslení formuláře
+  await page.waitForSelector('form');
 
-  return {
-    //out,
-    result
+  // Vybere dle id jednotlivé hodnoty
+  await page.select('select#ageYear', '1990'); // Rok
+  await page.select('select#ageMonth', '01');  // Měsíc
+  await page.select('select#ageDay', '01');    // Den
 
-  };
+  // Submit the form
+  //await page.waitForSelector('#view_product_page_btn');
+  
+  // Klikne na ověření věku
+  await page.click('#view_product_page_btn');
+
+  // Počká až se stránka přesměruje
+  await page.waitForNavigation();
+
+  // Vybere data na základě parametrů
+  const gameName = cleanText(await page.evaluate(element => element.textContent, await page.$('#appHubAppName'))); // id
+  const description = cleanText(await page.evaluate(element => element.textContent, await page.$('.game_description_snippet'))); // třída
+  
+  // Vybere skupinu elementů v divu
+  const tags = await page.evaluate(() => {
+      const elements = Array.from(document.querySelectorAll('.glance_tags.popular_tags .app_tag'));
+      return elements.slice(0, -1).map(tag => tag.textContent.trim());
+  });
+
+  // Vybere skupinu elementů v divu
+  const reguirements = await page.evaluate(() => {
+      return Array.from(document.querySelectorAll('.game_area_sys_req_rightCol li')).map(tag => tag.textContent.trim());
+  });
+  //await page.screenshot({ path:'test.png'});
+
+  await browser.close();
+
+  return { url,gameName,description,tags, reguirements };
 };
 
-const fetchAgeVerifiedPage = async (url) => {
-    try {
-      //Inicializatce úvodní stránky
-      const initialResponse = await axios.get(url, {
-        headers: {
-          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36'
-        }
-      });
-  
-      // Data na odeslání
-      const formData = {
-        ageDay: '1',
-        ageMonth: 'January',
-        ageYear: '1990'
-      };
-  
-      // Výběr potředných cookies
-      const cookies = initialResponse.headers['set-cookie'].join('; ');
-  
-      // Post request na formulář s datem narození
-      const postResponse = await axios.post(url, new URLSearchParams(formData), {
-        headers: {
-          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36',
-          'Cookie': cookies,
-          'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        maxRedirects: 0,
-        validateStatus: status => status >= 200 && status < 303
-      });
-  
-      //Fetch přesměrování
-      const redirectedUrl = postResponse.headers.location;
-      const finalResponse = await axios.get(redirectedUrl, {
-        headers: {
-          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36',
-          'Cookie': cookies
-        }
-      });
-  
-      // Finální zpracování dat
-      const finalPage = cheerio.load(finalResponse.data).html();
-  
-      return { finalPage };
-    } catch (error) {
-      console.error(error);
-      return { error: error.message };
-    }
-  };
 
-// Hlavní funkce, který zavolá další funkci dle formátu url
+// Hlavní funkce, která zavolá další funkci dle formátu url
 const processDataBasedOnUrl = async (url) => {
   try {
-    const { data } = await axios.get(url);
+
+    const decodedUrl = decodeURIComponent(url);
+    
+    if (!validUrl.isHttpsUri(decodedUrl)) { //Kontroluje, zde je url ve správném formátu
+      throw new Error('Invalid URL');
+    }
+
+    const { data } = await axios.get(decodedUrl);
     const { document } = new JSDOM(data).window;
 
-    //TODO: Ověřit si správnost url př kontrola startswith - https://www.csfd.cz/
-    //TODO: Přidat url encode a decode
     let content;
     switch (true) {
-      case url.includes('csfd.cz'):
+      case decodedUrl.startsWith('https://www.csfd.cz/'):
         content = handleCsfdData(document);
         break;
-      case url.includes('myanimelist.net'):
+      case decodedUrl.startsWith('https://myanimelist.net/'):
         content = handleMalData(document);
         break;
-      case url.includes('store.steampowered.com'):
-        content = await handleSteamData(url);
+      case decodedUrl.startsWith('https://store.steampowered.com/'):
+        content = await handleSteamData(decodedUrl);
         break;
       default:
         content = { error: 'Unsupported URL type' };
